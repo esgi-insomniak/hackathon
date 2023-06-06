74fe389004271ae9afd02f1409602544c4e810b3
Kassim Mohamed <kassim91000@gmail.com>
2023-06-06 10:53:36 +0200

    Initiate feature forum

---
+++ client/app/forum/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+
+export default function Page() {
+    return (
+        <div className="flex justify-center items-center h-full">
+            <h1>Forum</h1>
+        </div>
+    );
+}
